9e16b953e4778a3ee4d04b0cc9e5e9ea25f39d73
kmarchana <archanak@qburst.com>
2017-07-03 15:45:59 +0530

    ts lint error fixed

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core"
+import { Component } from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: "<h1>Sompo Dashboard</h1><sompo-dash></sompo-dash>"
+  template: "<h1>Sompo Dashboard</h1><sompo-dash></sompo-dash>";
 })
 
 export class AppComponent { }
--- client/app/modules/dashboard/components/sample.component.ts
+++ client/app/modules/dashboard/components/sample.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core"
+import { Component } from "@angular/core";
 
 @Component({
     selector: "sompo-dash",
-    template: "<h1>Sample Data </h1>"
+    template: "<h1>Sample Data </h1>";
 })
 
 export class TestComponent {}
